33ef4ffa84be823b8cbc6db99b9f08e259755ff1
Jasmine <jasminecharles@hotmail.com>
2019-07-22 12:53:32 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowseRouter as Router }from 'react-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
+  
   <div>
     <Navigation />
   </div>
+
+  
 );
 
 export default App;
